02e472c070bb315b7900146a80cf4cd4e7c1fac2
AH Mangorangca <lavahotsnacks@gmail.com>
2025-06-24 14:36:59 +0800

    tried using route parameters
    - reference: https://youtu.be/QS8MwC8S4o8

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>`[id].vue` file</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
