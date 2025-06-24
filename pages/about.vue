58af49b223ffec8391c91da7d5f15c36682eb449
AH Mangorangca <lavahotsnacks@gmail.com>
2025-06-24 14:19:41 +0800

    explored how to add pages
    - deleted `app.vue` file so it would show the pages under the pages folder
    - disabled `devtools` under `nuxt.config.ts` file so it would stay on the page and not redirect it to error 500
    - reference: https://youtu.be/ovJfnoqUBk8

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <p>This page contains nothing</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>This page is the `index.vue` file under pages, a feature of Nuxt.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
